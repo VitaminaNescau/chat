49cbb23c42c61d7877fda36f92fc8a1289cccc70
VitaminaNescau <vitorjss82@gmail.com>
2023-05-10 06:25:42 -0300

    ok

---
+++ teste.java
@@ -0,0 +1,10 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class teste {
+    public static void main(String[] args) {
+        ServerSocket sc = new ServerSocket();
+        server.bind(new InetSocketAddress("192.168.20.209", 3030));  
+       
+    }
+}
