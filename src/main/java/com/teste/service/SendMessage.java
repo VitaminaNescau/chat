086a68e595b446de7c94759067429d521a69ba5c
VitaminaNescau <vitorjss82@gmail.com>
2023-05-10 06:25:26 -0300

    melhorei o codigo

--- src/main/java/com/teste/service/SendMessage.java
+++ src/main/java/com/teste/service/SendMessage.java
@@ -2,8 +2,11 @@ package com.teste.service;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
+
+import com.teste.configuration.ManagerUser;
 import com.teste.dto.FriendDTO;
 import com.teste.dto.Userdto;
 
@@ -14,18 +17,23 @@ public class SendMessage {
     private PrintWriter output;
     final String group = "GROUP";
 
+
+    private static  SendMessage instance;
     public SendMessage(){
         
     }
-    public void forwardMessage(ConcurrentHashMap<String,FriendDTO> users,Userdto user,String msg){
+    public static SendMessage getInstance(){
+        if (instance == null) {
+            instance = new SendMessage();        
+        }
+        return instance;
+    }
+    public void forwardMessage(HashMap<String, FriendDTO> users,Userdto user,String msg){
         String MSG[]  = verifySend(msg);
         
-        System.out.println(users.isEmpty()+" "+user.getUsername()+" "+msg);
             if (MSG[0].equals(group)) {
-                System.out.println(1);
                sendAll(users, user, MSG);   
             }else{
-                 System.out.println(2);
                 sendFor(users,MSG);
                
             }
@@ -33,7 +41,7 @@ public class SendMessage {
       
     }
     /*metodo para as mensagens em grupo*/
-    public void sendAll(ConcurrentHashMap<String,FriendDTO> users,Userdto userdto,String msg[]){
+    public void sendAll(HashMap<String, FriendDTO> users,Userdto userdto,String msg[]){
         users.forEach((c,v)->{
             System.out.println(v.getUsername());
             try {
@@ -47,7 +55,7 @@ public class SendMessage {
         });
     }
     /*metodo para as mensagens privadas*/
-    public void sendFor(ConcurrentHashMap<String,FriendDTO> users,String msg[]){
+    public void sendFor(HashMap<String, FriendDTO> users,String msg[]){
         try {
             output = new PrintWriter(users.get(msg[0]).getSocket().getOutputStream(), true);
             output.println(msg[1]);
