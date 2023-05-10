b4fd492bf3cd6e767969ddee32e7c9ebf9e6e1fb
VitaminaNescau <vitorjss82@gmail.com>
2023-05-10 06:25:36 -0300

    agora é uma thread

--- src/main/java/com/teste/service/FriendsAndGroups.java
+++ src/main/java/com/teste/service/FriendsAndGroups.java
@@ -3,18 +3,31 @@ package com.teste.service;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
+import com.teste.configuration.ManagerUser;
+import com.teste.configuration.ServerDK;
 import com.teste.dao.User_dao;
 import com.teste.dto.FriendDTO;
 import com.teste.dto.Userdto;
+import com.teste.model.Usermodel.Status;
 
 public class FriendsAndGroups extends Thread{
-    FriendDTO friendDTO;
+    private FriendDTO friendDTO;
+    private Userdto user;
+    private ManagerUser accounts;
+    public  HashMap<String,FriendDTO> users = new HashMap<>();
     
-    public FriendsAndGroups() {}
+    public FriendsAndGroups(Userdto user,ManagerUser accounts){
+        this.user = user;
+        this.accounts = accounts;
+    }
+    public FriendsAndGroups(){
 
+    }
     // public Userdto addFriend(String name){
     //     Usermodel model = User_dao.getInstance().findUser(name);
     //     Userdto friend = new Userdto();
@@ -26,8 +39,10 @@ public class FriendsAndGroups extends Thread{
     //     friend.setHost(model.getHost());
     //     return friend;
     // };
-    public void joinGroup(){}
-    public List<FriendDTO> allFriends(Userdto user){
+    public void joinGroup(){
+       
+    }
+    public List<FriendDTO> allFriends(){
         List<FriendDTO> friendList = new ArrayList<>();
         User_dao.getInstance().findFriend(user).forEach(e->{
             friendDTO = new FriendDTO();
@@ -39,14 +54,31 @@ public class FriendsAndGroups extends Thread{
          });
        return friendList;
     }
-    public Hashtable friend(){
-        
-        return null;}
 
     @Override
     public void run() {
         while (true) {
-            
+       
+        allFriends().forEach(e->{
+                users.put(e.getUsername(),e);
+                try {
+                    if(accounts.findUser(e.getUsername()) != null){
+                        users.get(e.getUsername()).setSocket(accounts.findUser(e.getUsername()).getSocket());
+                    }
+                } catch (NullPointerException erro) {
+                   users.get(e.getUsername()).setStatus(Status.OFF);
+                }
+
+            });
+            //System.out.println(users);
+              try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+       
+        }
+       
     }
 }
