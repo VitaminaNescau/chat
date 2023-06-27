d58181cc3337b8e062a573f699618c16d7131295
VitaminaNescau <vitorjss82@gmail.com>
2023-06-27 07:06:10 -0300

    endpoint view old messager

---
+++ src/main/java/com/teste/controller/MessagerController.java
@@ -0,0 +1,29 @@
+package com.teste.controller;
+
+import java.util.List;
+
+import com.teste.dto.MessagerDTO;
+import com.teste.service.FriendsAndGroups;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Path("messager")
+@Produces(MediaType.APPLICATION_JSON)
+public class MessagerController {
+    @GET
+    @Path("/{id}/{name}")
+    public Response findMessager(@PathParam("id") int id,@PathParam("name") String name){
+        List<MessagerDTO> messager = new FriendsAndGroups().MessagerHistory(id,name);
+        if (!messager.isEmpty()) {
+        return Response.status(Status.ACCEPTED).entity(messager).build();
+        }
+        return Response.status(Status.NO_CONTENT).entity("Não foi encontrado dados ").build();
+    }
+
+}
