3f9cc79cc151fa1e5579f3a9c0fc8d6037c6692e
VitaminaNescau <vitorjss82@gmail.com>
2023-06-27 07:04:39 -0300

    class with controllers the api

--- src/main/java/com/teste/configuration/Controller.java
+++ src/main/java/com/teste/configuration/Controller.java
@@ -3,7 +3,10 @@ package com.teste.configuration;
 import java.util.HashSet;
 import java.util.Set;
 
+
 import com.teste.controller.FriendController;
+import com.teste.controller.MessagerController;
+import com.teste.controller.Security;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
@@ -14,6 +17,8 @@ public class Controller extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new HashSet<>();
         classes.add(FriendController.class);
+        classes.add(MessagerController.class);
+        classes.add(Security.class);
         return classes;
     }
 
