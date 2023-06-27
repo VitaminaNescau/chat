d2c05bed779c40d205392b580ac0b86f6d7da0dd
VitaminaNescau <vitorjss82@gmail.com>
2023-06-27 07:06:55 -0300

    add FindUser and AddFriend

--- src/main/java/com/teste/dao/User_dao.java
+++ src/main/java/com/teste/dao/User_dao.java
@@ -15,6 +15,7 @@ import com.teste.model.MessagerModel;
 import com.teste.model.Usermodel;
 import com.teste.model.Usermodel.Status;
 
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.FlushModeType;
@@ -30,8 +31,7 @@ public class User_dao {
     private ThreadPoolExecutor poll = new ThreadPoolExecutor(0,
     Integer.MAX_VALUE,10,TimeUnit.SECONDS,new SynchronousQueue<>());
     public User_dao(){
-        manager = conectionDB();
-        
+        manager = conectionDB();   
     }
     public static User_dao getInstance(){
         if (instance == null) {
@@ -50,22 +50,26 @@ public class User_dao {
         
         return manager;
     }
-    public boolean createUser(){
+    public boolean createUser(Usermodel user){
           
             manager.getTransaction().begin();
-            Query query = manager.createNativeQuery("select nome,hostname from usuario where nome = :username or hostname = :host")
-                .setParameter("username", user.getUsername())
-                .setParameter("host", user.getHost());
+            Query query = manager.createNativeQuery("select nome,hostname from usuario where nome = :username")
+                .setParameter("username", user.getUsername());
                 if (query.getResultList().isEmpty()) {
-                    manager.persist(user);
+                    try {
+                         manager.persist(user);
+                    } catch (EntityExistsException e) {
+                        
+                    }
+                   
                     Logger.getLogger("CREATE").info("USUARIO REGISTRADO");
                      manager.getTransaction().commit();
-                     manager.close();
+                     
                     return true;
                 }else{
                     Logger.getLogger("CREATE").info("USUARIO JA EXISTE NO BANCO");
                     manager.getTransaction().commit();
-                    manager.close();
+                    
                     return false;
                 }
         
@@ -77,25 +81,45 @@ public class User_dao {
         manager.getTransaction().commit();
         manager.close();
     }
-    public FriendDTO findUser(String name){
+    public Usermodel findUser(String name){
         
-        Query query = manager.createNativeQuery("Select nome,hostname,status  from usuario where nome = :username")
+        Query query = manager
+        .createNativeQuery("Select * from usuario where nome = :username",Usermodel.class)
         .setParameter("username",name);
         manager.getTransaction().begin();
     
         if (!query.getResultList().isEmpty()) {
-            FriendDTO result = (FriendDTO) query.getResultList().iterator().next();
-             manager.getTransaction().commit();
-             return result;
+            Usermodel resultModel =  (Usermodel)query.getResultList().iterator().next();
+            manager.getTransaction().commit();
+        // FriendDTO result = new FriendDTO();
+           
+            return resultModel;
         }
         manager.getTransaction().commit();
         return null;
     }
+        public FriendDTO findUser(String name,boolean verify){
+        if (verify) {
+              Query query = manager
+        .createNativeQuery("Select * from usuario where nome = :username",Usermodel.class)
+        .setParameter("username",name);
+        manager.getTransaction().begin();
+    
+        if (!query.getResultList().isEmpty()) {
+            Usermodel resultModel =  (Usermodel)query.getResultList().iterator().next();
+            manager.getTransaction().commit();
+            FriendDTO result = new FriendDTO();
+           result.setUsername(resultModel.getUsername());
+           result.setStatus(resultModel.getStatus());
+           result.setId(resultModel.getId_username().intValue());
+            return result;
+        }
+        manager.getTransaction().commit();
+        }
+        return null;
+    }
     public List<Friendmodel> findFriend(int id){
-       
         Query query = manager.createNativeQuery("Select * from amigos where id_user = :id",Friendmodel.class).setParameter("id", id);
-     
-       
         try {
             return poll.submit(()->{
             List<Friendmodel> result = new ArrayList<>();    
@@ -107,19 +131,9 @@ public class User_dao {
             }).get();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
-        }
-        
-        
+        } 
        return null;
-       
-       
-       
-        
-      
-        
-        
-    
-     
+
     }
     public Usermodel loginUser(Usermodel user){
        
@@ -150,11 +164,19 @@ public class User_dao {
     }
     public List<MessagerModel> MessagerHistory(int id){
         Query query = manager.createNativeQuery("Select * from mensagem where send_id = :id or receiver_id = :id",MessagerModel.class).setParameter("id",id);
-        manager.getTransaction().begin();
-        
+        manager.getTransaction().begin();  
         List<MessagerModel> result = query.getResultList();
-
         manager.getTransaction().commit();
         return result;
     }
+    public void AddFriend(String nameFriend,String nameUser){
+        Friendmodel friendmodel = new Friendmodel();
+        friendmodel.setId_friend(findUser(nameFriend));
+        //friendmodel.setId_user(()->{t});
+        friendmodel.setId_user(findUser(nameUser));
+        manager.getTransaction().begin();
+            manager.persist(friendmodel);
+        manager.getTransaction().commit();
+    }
+
 }
