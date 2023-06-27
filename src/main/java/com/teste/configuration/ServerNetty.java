b34288c372e4e9f2e3c13388e7b650222977a8f4
VitaminaNescau <vitorjss82@gmail.com>
2023-06-27 07:05:17 -0300

    class in the serve

--- src/main/java/com/teste/configuration/ServerNetty.java
+++ src/main/java/com/teste/configuration/ServerNetty.java
@@ -1,14 +1,12 @@
 package com.teste.configuration;
 
-
 import org.jboss.resteasy.core.ResteasyDeploymentImpl;
 import org.jboss.resteasy.plugins.server.reactor.netty.ReactorNettyJaxrsServer;
 import org.jboss.resteasy.spi.ResteasyDeployment;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
-import jakarta.ws.rs.ext.ExceptionMapper;
 
-
+// import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainerProvider;
+// import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+// import org.glassfish.jersey.server.ResourceConfig;
 
 public class ServerNetty {
     
@@ -18,19 +16,14 @@ public class ServerNetty {
         ResteasyDeployment dp = new ResteasyDeploymentImpl();
         dp.setApplicationClass(Controller.class.getName());
         server.setDeployment(dp);
+        server.setHostname("localhost");
         server.setPort(8080);
         server.start();
-    
+        // GrizzlyHttpServerFactory
+        // .createHttpServer(URI.create("http://localhost:8080/"),
+        // new ResourceConfig().packages("com.teste.controller"));
+        
 }
 } 
- class ExceptionMappingProvider implements ExceptionMapper<Exception> {
-        @Override
-        public Response toResponse(Exception exception) {
-            if (exception instanceof IllegalStateException) {
-                return Response.status(Status.BAD_REQUEST).build();
-            }
-            return Response.serverError().build();
-        }
 
-    }
 
